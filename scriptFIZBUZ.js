386e32ee1daa10d7951a0a2a983b1d1834470ae2
shirleyciom <shirleyciom@live.com>
2021-10-13 09:00:48 +0100

    Java Part5 Arrays JSON Object challange

---
+++ scriptFIZBUZ.js
@@ -0,0 +1,16 @@
+
+for (let i=1; i < 101; i++){
+   
+   if (i % 3 == 0) console.log("Fizz");
+    else if (i % 5 == 0) console.log("Buzz");
+    else if (i % 15 == 0) console.log("FizzBuzz");
+    else console.log(i);
+}
+
+/* 
+Write a program that will console log integers 1 to 100
+but print “Fizz” if an integer is divisible by 3
+“Buzz” if an integer is divisible by 5
+and “FizzBuzz” if an integer is divisible by both 3 and 5
+upload your work to GitHub
+*/
