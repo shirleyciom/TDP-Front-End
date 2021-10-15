3028b5dc7fce634e50db6130d1bac08bbcda1166
shirleyciom <shirleyciom@live.com>
2021-10-15 14:48:22 +0100

    Java Json

---
+++ scriptStructure.js
@@ -0,0 +1,26 @@
+/*
+**Script 
+
+Break up code into 3 sections 
+ -variable 
+
+let element = document.query 
+
+ -methods - are actions we want to perform
+ button 
+ let handle click = function {
+     console.log(event) 
+ }
+This links to the event listner 
+
+
+ -event listners (these are things like clicking typing text entry) 
+element.addeventlistner 
+
+Code should be broken up the same as c# does with classes etc 
+
+
+DOM - Document Object Model 
+*
+
+*/
